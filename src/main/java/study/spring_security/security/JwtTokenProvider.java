76f7a8c841f8f72211889ea4c9f08ca825600ad4
ddohyeong <dohyeong7026@gmail.com>
2024-05-03 10:09:05 +0900

    feat: jwtToken 을 추출하는 메서드 구현 및 토큰의 expired를 체크하는 메서드 구현

--- src/main/java/study/spring_security/security/JwtTokenProvider.java
+++ src/main/java/study/spring_security/security/JwtTokenProvider.java
@@ -1,12 +1,15 @@
 package study.spring_security.security;
 
 import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -30,6 +33,27 @@ public class JwtTokenProvider {
 	private Key getSigningKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(env.getProperty("JWT.SECRET"));
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+	private boolean isTokenExpired(String token) {
+		return extractExpired(token).before(new Date());
+	}
+
+	private Date extractExpired(String token) {
+		return extractClaim(token, Claims::getExpiration);
 	}
 
 }
