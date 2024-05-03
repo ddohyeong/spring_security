87af7952d0ed9c4974289a37ff7bc8c5f9f57ac6
ddohyeong <dohyeong7026@gmail.com>
2024-05-03 12:19:04 +0900

    feat: BaseExceptionHandler 으로 TestController 수정

--- src/main/java/study/spring_security/TestController.java
+++ src/main/java/study/spring_security/TestController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import study.spring_security.common.exception.BaseException;
+import study.spring_security.common.response.BaseResponse;
+import study.spring_security.common.response.BaseResponseStatus;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 	@GetMapping
-	public void test(@RequestHeader("Authorization") String token) {
+	public BaseResponse<Void> test(@RequestHeader("Authorization") String token) {
 		System.out.println(token);
+
+		return new BaseResponse<>();
 	}
 }
