b95eef07b5f5872c60204bea26ef2157fd75b6d0
ddohyeong <dohyeong7026@gmail.com>
2024-05-05 13:30:16 +0900

    feat: exception handler에서 jwt token 만료 예외가 처리되는지 확인 메서드 추가

--- src/main/java/study/spring_security/TestController.java
+++ src/main/java/study/spring_security/TestController.java
@@ -3,6 +3,7 @@ package study.spring_security;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import study.spring_security.common.exception.BaseException;
@@ -13,9 +14,12 @@ import study.spring_security.common.response.BaseResponseStatus;
 @RequestMapping("/test")
 public class TestController {
 	@GetMapping
-	public BaseResponse<Void> test(@RequestHeader("Authorization") String token) {
+	public BaseResponse<Void> test(@RequestHeader("Authorization") String token,
+			@RequestParam("test") String test) {
 		System.out.println(token);
-
+		if (test.equals("error")) {
+			throw new BaseException(BaseResponseStatus.WRONG_JWT_TOKEN);
+		}
 		return new BaseResponse<>();
 	}
 }
